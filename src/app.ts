b39582f4d2d1e550afa00b919dd10f1018a38999
minyeok <rltkeh150@naver.com>
2026-03-06 18:38:59 +0900

    refactor:업로드 폴더 접근 허용 경로 변경

--- src/app.ts
+++ src/app.ts
@@ -12,7 +12,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
-app.use('/uploads', express.static(path.join(__dirname, '../uploads')));
+app.use('/uploads', express.static(path.join(process.cwd(), 'uploads')));
 
 app.use('/api/auth', authRouter);
 app.use('/api/complaints', complaintRouter);
